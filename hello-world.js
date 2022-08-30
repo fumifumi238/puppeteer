49a3be520a7383208f54966adfdb39575ad54cdf
epthalite <fmy628.1998@icloud.com>
2022-08-30 21:13:19 +0900

    add selector[

--- hello-world.js
+++ hello-world.js
@@ -3,40 +3,22 @@ const puppeteer = require("puppeteer");
 (async () => {
   // 0時にはリセットする
   //　過ぎた時間は削除
+  // クーポン追加するとクッキーがいるので無効になる
+  // カット以外は無効
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(
-    "https://beauty.hotpepper.jp/CSP/bt/reserve/?storeId=H000257369&couponId=CP00000005849082&add=2&addMenu=0&rootCd=10&wak=BPCO104005_button_style_coupon"
-  );
+  const url =
+    "https://beauty.hotpepper.jp/CSP/bt/reserve/?storeId=H000255127&couponId=CP00000006479077&add=2&addMenu=0&rootCd=10";
+  await page.goto(url);
 
-  const fetchText = async (xPath) => {
-    const path = await page.$x(xPath);
+  const fetchText = async (selector) => {
+    const path = await page.$$(selector);
     const jsHandle = await path[0].getProperty("textContent");
     const text = await jsHandle.jsonValue();
     const list = text.trim().replace(/\s+/g, " ").split(" ");
 
     return list;
   };
-  const days = await fetchText('//*[@id="jsRsvCdTbl"]/table/thead/tr[2]');
-
-  console.log(days);
-
-  const times = await fetchText(
-    '//*[@id="jsRsvCdTbl"]/table/tbody/tr/th[1]/table'
-  );
-
-  console.log(times);
-  const column = [];
-
-  for (let i = 0; i < times.length; i++) {
-    row = [];
-    for (let j = 0; j < days.length; j++) {
-      row.push("－");
-    }
-    column.push(row);
-  }
-
-  const schedules = await fetchText('//*[@id="jsRsvCdTbl"]/table/tbody/tr');
 
   const trimHoliday = (text) => {
     const num = schedules.indexOf("休業日");
@@ -49,6 +31,22 @@ const puppeteer = require("puppeteer");
 
     return trimHoliday(text);
   };
+
+  const days = await fetchText(".dayCellContainer");
+
+  // console.log(days);
+
+  const times = await fetchText(".timeTableLeft");
+
+  // console.log(times);
+  const column = [];
+
+  for (let i = 0; i < times.length; i++) {
+    row = new Array(days.length);
+    column.push(row);
+  }
+
+  const schedules = await fetchText("#jsRsvCdTbl > table > tbody > tr");
 
   const trimSchedules = trimHoliday(schedules);
 
